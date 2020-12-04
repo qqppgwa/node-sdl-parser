5e333b1049f441dcaaaf8e00e24fb1f0c5f65c5a
qqppgwa <qqppgwa@gmailcom>
2020-12-05 02:50:37 +0800

    refactor: 💡 modify createDummy in appendDummy.ts
    Add `\n` and `\t` into the returned value to make it be more like
    original data.

--- test/utils/appendDummy.ts
+++ test/utils/appendDummy.ts
@@ -1,20 +1,19 @@
-import fs from "fs";
+import fs from 'fs';
 
 const SIZE = 5000;
 
 function createDummy(index: number) {
-  return `TOKEN
-  Name  = "DUMMY${index}"
-  Value  = "0x0"
-  TokenType = Integer
-  TargetMAK = Yes
-  TargetH = Yes
-  TargetEQU = Yes
-End\n`;
+  return `TOKEN\n
+  \tName  = "DUMMY${index}"\n
+  \tValue  = "0x0"\n
+  \tTokenType = Integer\n
+  \tTargetMAK = Yes\n\tTargetH = Yes\n
+  \tTargetEQU = Yes\n
+  End\n`;
 }
 
 export default function appendDummy(fp: string) {
-  const logStream = fs.createWriteStream(fp, { flags: "a" });
+  const logStream = fs.createWriteStream(fp, { flags: 'a' });
   for (let i = 0; i < SIZE; i += 1) {
     const dummy = createDummy(i);
     if (i !== SIZE - 1) {
